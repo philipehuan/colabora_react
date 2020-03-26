82c32b74441da7560d9bbd8ca5d79cb1c758fc7e
philipehuan <philipehuan@live.com>
2020-03-26 20:24:20 -0300

    commit of 26/03

--- src/routes/Departamentos.js
+++ src/routes/Departamentos.js
@@ -3,8 +3,24 @@ import React,{Component} from "react";
 import "../routes/App.css";
 import Navbar_brand from "../components/navbar_brand";
 import MenuBar from "../components/menuBar";
+import axios from 'axios';
 
 class Departamentos extends Component{
+    state={
+        departamento:[]
+    }
+
+    componentDidMount() {
+        axios({
+            method:'get',
+            url:'http://localhost:8000/colabora/coreapp/departamento/',
+            responseType:'json'
+        })
+            .then(res=>{
+                const departamento = res.data;
+                this.setState({departamento})
+            })
+    }
 
     render() {
         return(
@@ -13,6 +29,10 @@ class Departamentos extends Component{
                     <Navbar_brand />
                     <MenuBar />
                 </header>
+                <ul>
+                    {this.state.departamento.map(dep=><li>{dep.nomedepartamento}</li>)}
+                    {this.state.departamento.map(dep2=><li>{dep2.id}</li>)}
+                </ul>
             </div>
         )
     }
